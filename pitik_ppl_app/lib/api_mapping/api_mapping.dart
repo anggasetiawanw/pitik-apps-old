53c65372b697afdf45151575d9974ad3dc0a9d94
Dicky Maulana Akbar <akbar.attijani@gmail.com>
2023-10-05 16:02:23 +0700

    add api mapping

---
+++ pitik_ppl_app/lib/api_mapping/api_mapping.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:engine/request/api_mapping_list.dart';
+
+import '../../flavors.dart';
+import 'apis/api.dart';
+import 'apis/smart_scale_api.dart';
+import 'apis/user_api.dart';
+
+///@author DICKY
+///@email <dicky.maulana@pitik.idd>
+///@create date 11/09/2023
+
+class ApiMapping extends ApiMappingList {
+
+    Map<String, Type> apiMapping = {
+        "api": API,
+        "userApi": UserApi,
+        "smartScaleApi": SmartScaleApi
+    };
+
+    @override
+    Type getApiMapping(String apiKey) {
+        return apiMapping[apiKey]!;
+    }
+
+    @override
+    String getBaseUrl() {
+        return F.uri;
+    }
+}
